a3042b73d0ad5d0eadac2a6a50aae3f6c12fa21d
petrov bogdan <bogdan_emdetei_petrov@mail.ru>
2023-11-18 17:13:51 +0300

    process

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ import { createComments, deleteComment, getAllComments, getAllPost } from "./con
 // 'mongodb+srv://admin:Qwer_1234@cluster0.wy8ihwv.mongodb.net/blog2?retryWrites=true&w=majority'
 //подключение базы данных mongodb
 mongoose.connect(
-    procces.env.mongodb_api
+    process.env.mongodb_API
 ).then(() => console.log('db ok'))
     .catch((err) => console.log('db err', err))
 
@@ -89,7 +89,7 @@ app.get('/posts/:postId/comments', getAllPost);
 
 
 //создание порта
-app.listen(procces.env.PORT || 4444, (err) => {
+app.listen(process.env.PORT || 4444, (err) => {
     if (err) {
         return console.warn(err)
     }
