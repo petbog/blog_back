9b669c1e3f19fabea62ef1505729e673cd98fa14
petrov bogdan <bogdan_emdetei_petrov@mail.ru>
2023-11-18 23:12:45 +0300

    v3

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ import { createComments, deleteComment, getAllComments, getAllPost } from "./con
 // 'mongodb+srv://admin:Qwer_1234@cluster0.wy8ihwv.mongodb.net/blog2?retryWrites=true&w=majority'
 //подключение базы данных mongodb
 mongoose.connect(
-    process.mongodb_API
+    mongodb_API
 ).then(() => console.log('db ok'))
     .catch((err) => console.log('db err', err))
 
