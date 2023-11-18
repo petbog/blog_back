21ac1ef49deb027c24cd37bfdab2c0caffd04d8e
petrov bogdan <bogdan_emdetei_petrov@mail.ru>
2023-11-18 17:06:18 +0300

    vercel

--- index.js
+++ index.js
@@ -4,17 +4,16 @@ import mongoose from "mongoose"
 import cors from 'cors'
 import checkAuth from "./Utils/checkAuth.js"
 import { getMe, login, register } from "./controllers/UserController.js"
-import { create, getAll, getLastTags, getNewPost, getOnePost,  getPopulatePost,  getPostsByTag,  removePost, update } from "./controllers/PostController.js"
+import { create, getAll, getLastTags, getNewPost, getOnePost, getPopulatePost, getPostsByTag, removePost, update } from "./controllers/PostController.js"
 import multer from "multer"
 import handleValidationErrors from './Utils/handleErrors.js'
 import { createComments, deleteComment, getAllComments, getAllPost } from "./controllers/CommentsController.js"
 
 
-
-
+// 'mongodb+srv://admin:Qwer_1234@cluster0.wy8ihwv.mongodb.net/blog2?retryWrites=true&w=majority'
 //подключение базы данных mongodb
 mongoose.connect(
-    'mongodb+srv://admin:Qwer_1234@cluster0.wy8ihwv.mongodb.net/blog2?retryWrites=true&w=majority'
+    procces.env.mongodb_api
 ).then(() => console.log('db ok'))
     .catch((err) => console.log('db err', err))
 
@@ -46,13 +45,13 @@ app.use(cors())
 //авторизация 
 app.post('/auth/login', loginValidation, handleValidationErrors, login)
 //регистрация
-app.post('/auth/register',  registerValidation, handleValidationErrors, register)
+app.post('/auth/register', registerValidation, handleValidationErrors, register)
 //инфо о нас
 app.get('/auth/me', checkAuth, getMe)
 
 
 //загрузка картинки
-app.post('/upload',  upload.single('image'), (req, res) => {
+app.post('/upload', upload.single('image'), (req, res) => {
     res.json({
         url: `/uploads/${req.file.originalname}`
     })
@@ -74,13 +73,13 @@ app.get('/tags', getLastTags)
 //получение  статей по тегу 
 app.get('/articlesbytag', getPostsByTag)
 //получение статей по популярности
-app.get('/populate',getPopulatePost)
+app.get('/populate', getPopulatePost)
 //сортировка постов новые
-app.get('/new',getNewPost)
+app.get('/new', getNewPost)
 
 
 //создание комментария
-app.post('/posts/:postId/comment', commentsValidation,createComments);
+app.post('/posts/:postId/comment', commentsValidation, createComments);
 //получение всех комментариев
 // app.get('/posts/:postId/comments', commentsValidation,getAllComments);
 //удаление коомментария
@@ -90,7 +89,7 @@ app.get('/posts/:postId/comments', getAllPost);
 
 
 //создание порта
-app.listen(4444, (err) => {
+app.listen(procces.env.PORT || 4444, (err) => {
     if (err) {
         return console.warn(err)
     }
