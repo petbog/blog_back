deb7f88c246fd4651b07d56cc20efeaa0477dafd
Petrov Bogdan <bogdan_emdetei_petrov@mail.ru>
2023-11-20 00:50:20 +0300

    link

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cors())
 config()
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://frontblog-three.vercel.app');
+    res.setHeader('Access-Control-Allow-Origin', 'https://frontblog-phi.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
