8e64d9bb7526bba680946b8ce586710fcb5821cb
Petrov Bogdan <bogdan_emdetei_petrov@mail.ru>
2023-11-19 21:31:33 +0300

    link

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.json())
 app.use(cors())
 config();
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://blog-back-ft8xz6t59-bogdans-projects-232d6ed4.vercel.app/');
+    res.setHeader('Access-Control-Allow-Origin', 'https://blog-back-phi.vercel.app/');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
