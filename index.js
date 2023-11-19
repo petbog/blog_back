8041b7e9563b89f133be445509e0e10fbe561d15
Petrov Bogdan <bogdan_emdetei_petrov@mail.ru>
2023-11-19 21:45:30 +0300

    /

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.json())
 app.use(cors())
 config();
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://blog-back-phi.vercel.app/');
+    res.setHeader('Access-Control-Allow-Origin', 'https://blog-back-phi.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
