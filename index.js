b610c24cebd040b02f40a08a65ae32ae58057794
petrov bogdan <bogdan_emdetei_petrov@mail.ru>
2023-11-21 14:41:46 +0300

    api

--- index.js
+++ index.js
@@ -17,11 +17,11 @@ const app = express();
 const PORT = '4444';
 app.use(express.json());
 app.use(cors({
-    origin: 'https://frontblog-phi.vercel.app' 
+    origin: 'https://frontblog-nine.vercel.app' 
   }));
 config()
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://frontblog-phi.vercel.app');
+    res.setHeader('Access-Control-Allow-Origin', 'https://frontblog-nine.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
