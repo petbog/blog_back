01d7a2699d55fcd4d2978a4cb18d4932aac23fe8
petrov bogdan <bogdan_emdetei_petrov@mail.ru>
2023-11-21 15:43:20 +0300

    npm

--- index.js
+++ index.js
@@ -18,8 +18,7 @@ const PORT = '4444';
 app.use(express.json());
 app.use(cors());
 config()
-app.use((req, res, next) => {
-    
+app.use((req, res, next) => { 
     res.setHeader('Access-Control-Allow-Origin', 'https://frontblog-delta.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
